d5659c1e4a040e42f172755b93b351f610767f5f
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-04-19 23:17:27 -0600

    Importa Componente de Mensaje y utiliza props para mostrar mensaje de presupuesto valido

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Mensaje from "./Mensaje";
+
 
 const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+  const [mensaje, setMensaje] = useState("");
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    if (!Number(presupuesto) || Number(presupuesto) < 0) {
+      setMensaje("No es un presupuesto valido");
+    } else {
+      console.log("Si es un presupuesto valido");
+    }
+  };
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-      <form className="formulario">
+      <form onSubmit={handlePresupuesto} className="formulario">
         <div className="campo">
           <label>Definir Presupuesto</label>
           <input
@@ -16,6 +28,9 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
         </div>
 
         <input type="submit" value="Añadir" />
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
       </form>
     </div>
   );
