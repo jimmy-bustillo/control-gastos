4c010fff4f224e16f39e26a09914e8fef9907f16
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-04-22 12:46:59 -0600

    Muestra el Modal  al hacer click en el icono de Nuevo Gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
+
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  };
   return (
     <div>
       <Header
@@ -16,9 +23,15 @@ function App() {
 
       {isValidPresupuesto && (
         <div className="nuevo-gasto">
-          <img src={IconoNuevoGasto} alt="Icono de Nuevo Gasto" />
+          <img
+            src={IconoNuevoGasto}
+            alt="Icono de Nuevo Gasto"
+            onClick={handleNuevoGasto}
+          />
         </div>
       )}
+
+      {modal && <Modal setModal={setModal} />}
     </div>
   );
 }
