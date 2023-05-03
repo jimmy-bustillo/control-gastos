49262fc6dc072ba7561ede4b5df5c0a75d76791b
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-02 22:35:33 -0600

    Agrega clase para fijar el modal

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className={modal ? 'fijar' : ''}>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
