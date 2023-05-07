b3de85c1783a14d61c2d26ff4c5a3c4f4852c260
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-07 15:58:29 -0600

    Agrega localStorage para los gastos

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
-  const [gastos, setGastos] = useState([])
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  )
 
   const [gastoEditar, setGastoEditar] = useState({})
 
@@ -30,6 +32,10 @@ function App() {
   useEffect(() =>{
     localStorage.setItem('presupuesto', presupuesto ?? 0)
   },[presupuesto])
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+  }, [gastos])
 
   useEffect(() => {
     const presupuestoLS = Number(localStorage.getItem(`presupuesto`)) ?? 0
