8694d4b20a9c7f47f55b71e3f9164b56ef57fc8f
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-06 21:37:15 -0600

    Añade los props para editar y eliminar los gastos

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,12 +1,17 @@
 import Gasto from "./Gasto"
 
-const ListadoGastos = ({ gastos }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto }) => {
   return (
     <div className='listado-gastos contenedor'>
       <h2>{gastos.length > 0 ? "Gastos" : "No hay gastos aún"}</h2>
 
       {gastos.map((gasto) => (
-        <Gasto key={gasto.id} gasto={gasto} />
+        <Gasto
+          key={gasto.id}
+          gasto={gasto}
+          setGastoEditar={setGastoEditar}
+          eliminarGasto={eliminarGasto}
+        />
       ))}
     </div>
   )
